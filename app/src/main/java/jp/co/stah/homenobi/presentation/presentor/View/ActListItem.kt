3ce0867fab23046b5996d101c3e2b1369c6179cf
yoshida0261 <yoshida261@gmail.com>
2018-12-16 01:25:07 +0900

    feat:primary, primarydrakを設定

---
+++ app/src/main/java/jp/co/stah/homenobi/presentation/presentor/View/ActListItem.kt
@@ -0,0 +1,3 @@
+package jp.co.stah.homenobi.presentation.presentor.View
+
+data class ActListItem(val topic: String, val date: String, val episode: String)
--- app/src/main/java/jp/co/stah/homenobi/presentation/presentor/View/TweetListItem.kt
+++
@@ -1,3 +0,0 @@
-package jp.co.stah.homenobi.presentation.presentor.View
-
-data class TweetListItem(val topic: String, val date: String, val episode: String)
