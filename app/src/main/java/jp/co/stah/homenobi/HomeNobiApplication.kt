93b9c2622225f814b2b2d4d0b9ebea3606145bc6
yoshida0261 <yoshida261@gmail.com>
2018-12-18 20:08:28 +0900

    feat: timber追加

--- app/src/main/java/jp/co/stah/homenobi/HomeNobiApplication.kt
+++ app/src/main/java/jp/co/stah/homenobi/HomeNobiApplication.kt
@@ -1,7 +1,14 @@
 package jp.co.stah.homenobi
 
 import android.app.Application
+import timber.log.Timber
 
-class HomeNobiApplication : Application(){
+class HomeNobiApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
 
 }
